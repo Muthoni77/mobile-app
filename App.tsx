d10132aace520eb4c351966a04ace96a6705af23
Muthoni77 <gloriamuthoni25@gmail.com>
2023-07-08 08:33:48 -0400

    header

--- App.tsx
+++ App.tsx
@@ -1,16 +1,38 @@
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import { StyleSheet, Text, View ,Pressable} from 'react-native';
+import {DefaultTheme, NavigationContainer, Theme} from "@react-navigation/native"
+// Import [LinearGradient ] from 'expo-linear-gradient';
 import RootNavigator from './src/Navigators/RootNavigator';
- 
+import { useMemo } from 'react';
+import { color } from 'react-native-reanimated';
+// import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
+ const theme: Theme= useMemo(()=>({...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    background : "#f5f5f5",
+    text:"#191919",
+    border:"#D9D9D9",
+    Primary:"#i91919"
+    
+   },
+   
+   
+}
+  ),
+ []
+ );
   return (
     <View style={styles.container}>
-     <NavigationContainer>
+     <NavigationContainer theme={theme}>
+   
+      
+     
                 <RootNavigator/>
                 <StatusBar style="dark"/>
+                {/* <Icon name="add-to-queue-button" color="red" /> */}
 
             </NavigationContainer>
     </View>
@@ -23,3 +45,4 @@ const styles = StyleSheet.create({
     
   },
 });
+
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
+import Icons from "@expo/vector-icons/MaterialIcons"
+
+const AVATAR_URL =
+  "https://clipart-library.com/images/ATbrxjpyc.jpg";
 
 const HomeScreen = () => {
+  const {colors}= useTheme()
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <SafeAreaView>
+        <View
+          style={{
+            paddingHorizontal: 24,
+            paddingVertical:24,
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 8,
+          }}
+        >
+          <Image
+            source={{ uri: AVATAR_URL }}
+            style={{ width: 32, aspectRatio: 1, borderRadius: 32 }}
+            resizeMode="cover"
+          />
+          <View style={{flex:1}}>
+            <Text style={{fontSize:18 , fontWeight:"600",marginBottom:8, color:colors.text}}
+            numberOfLines={1}
+            >Hi, John👋</Text>
+            <Text style={{color:colors.text, opacity:0.75}}
+            numberOfLines={1}>Discover Trading with us</Text>
 
-export default HomeScreen
+          </View>
+          <TouchableOpacity 
+          style={{
+            width:52,
+            aspectRatio:1,
+            alignItems:"center",
+            justifyContent:"center",
+            borderRadius:52,
+            borderWidth:1,
+            borderColor:colors.border,
+
+          }}
+          >
+            <Icons name="notifications" size={24} color={colors.text}/>
+
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
